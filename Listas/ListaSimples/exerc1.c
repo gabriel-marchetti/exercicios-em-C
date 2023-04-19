772d61fd4f1a9f1e45f9110738370d27397b57ef
Gabriel <gabrielcmarchetti@gmail.com>
2023-04-19 09:47:07 -0300

    Update exerc1

--- Listas/ListaSimples/exerc1.c
+++ Listas/ListaSimples/exerc1.c
@@ -14,6 +14,27 @@ typedef struct _node_list{
 } nodeList, simpleList;
 
 
+nodeList *createNode(int elem){
+    nodeList *node = (nodeList *)calloc(1, sizeof(nodeList));
+    node->elem = elem;
+    node->next = NULL;
+    return(node);
+}
+
+
+void destroyList(simpleList *start){
+    nodeList *p = start, *ant = NULL;
+    if (!(checkEmptyList(start))){
+        while (start->next != NULL){
+            p = start->next;
+            ant = start;
+            start = p;
+            free(ant);
+        }
+        free(p);
+    }
+}
+
 bool checkEmptyList(simpleList *start){
     if (start->next == NULL){
         return(true);
@@ -29,6 +50,7 @@ void printSimpleList(simpleList *start){
             printf("%d -->", start->elem);
             start = start->next;
         }
+        printf("%d\n", start->elem);
     }
 }
 
@@ -59,7 +81,37 @@ int main(void){
     while(1){
         printf("Entre com uma opcao: \n");
         printf("0 - Insere elemento no começo da lista.\n");
-    }
+        printf("1 - Insere elemento no final da lista\n");
+        printf("2 - Insere antes de uma chave.\n");
+        printf("\n");
 
+        scanf("%d", &option);
+        switch (option){
+        case 0:
+            printf("Adicione o elemento: "); scanf("%d", &elem);
+            if (head == NULL){
+                head = createNode(elem);
+            } else {
+                // Adiciono o elemento no começo da lista.
+                // Criar função que adiciona no começo da lista.
+
+            }
+            break;
+        case 1:
+            printf("Adicione o elemento: "); scanf("%d", &elem);
+            if (head == NULL){
+                head = createNode(elem);
+            } else {
+                // Adiciono o elemento no final da lista.
+                // Criar função que adiciona no final da lista.
+            }
+            break;
+        case 2:
+
+            break;
+        default:
+            break;
+        }
+    }
     return(0);
 }
