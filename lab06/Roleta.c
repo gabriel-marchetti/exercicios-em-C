969a91c674ff6cea795a052a5cb22f72749083db
Gabriel Cunha Marchetti <gabrielcmarchetti@gmail.com>
2023-05-14 18:45:35 -0300

    Update Roleta.c

--- lab06/Roleta.c
+++ lab06/Roleta.c
@@ -13,9 +13,14 @@ int main(int argc, char *argv[])
     int n;
     fscanf(file, "%d ", &n);
 
-    char action, test[] = { '0', '1', '2', '3', '4',
+    char action; 
+    
+    /*
+    test[] = { '0', '1', '2', '3', '4',
                             '5', '6', '7', '8', '9',
                             '+', 'D', 'C', 'I'} ; 
+    */
+   
     pilha *pil = NULL, *auxP;
     int aux;
     while(n){
