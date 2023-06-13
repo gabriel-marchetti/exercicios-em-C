d55332f1ca6af80e73716ee220b564c17ec56905
Gabriel <gabrielcmarchetti@gmail.com>
2023-06-13 13:21:05 -0300

    AugmentArray,
    Adicionei func AugmentArray.

--- heap/heap.c
+++ heap/heap.c
@@ -1,17 +1,54 @@
+#ifndef __HEAP__
+#define __HEAP__
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 
 /*--Estruturas-de-dados--*/
 
+typedef struct _arvbin{
+    int info;
+    struct _arvbin *left, *right;
+} bintree, nodeTree;
+
+
+typedef struct _linkedList{
+    int info;
+    struct _arvbin *next;
+} linkedlist;
+
+
+typedef struct _heap{
+    int tamMax, tam;
+    int *array[];
+} heap;
 
 /*-----------------------*/
 
-
 /*--Espaço das Funções--*/
+
 void heapify()
 {
 
+}
+
+
+int augmentArray(heap **Heap)
+{
+    int newSize = 2 * (*Heap)->tam;
+    int *newArray = realloc(*((*Heap)->array), newSize * sizeof(int));
+    if ( newArray = NULL ){
+        printf("Falha ao alocar a memória.\n");
+        return 0;
+    }
+    
+    *((*Heap)->array) = newArray;
+    (*Heap)->tamMax = newSize;
+    return 1;
 }
 
 /*---------------------*/
@@ -24,10 +61,23 @@ int main(int argc, char *argv[])
 
     FILE *file=NULL;
     int condition, continuar=1;
+    heap *Heap;
 
     /*---------------*/
 
     while ( continuar ){
+        /*--Condicao-Tamanho--*/
+        /*-Quero criar uma condição que verifica o tamanho do array do heap.*/
+        /*-Caso ele esteja muito cheio, crio um novo array e aloco para o heap.*/
+        if ( Heap->tamMax - Heap->tam < 3 ){
+            if ( augmentArray(&Heap) ) {
+                printf("Processo de Aumento do Heap realizado.\n");
+            } else {
+                printf("Problema no Aumento do Heap...\n");
+                return -1;
+            }
+        }
+
         // Options // 
         printf("----Operacoes----\n");
         printf("1 - Push no Heap.\n");
@@ -42,38 +92,52 @@ int main(int argc, char *argv[])
         printf("99 - Parar \n");
 
         // Switch //
+        printf("Qual opcao deseja realizar? ");
         fflush(stdin);
-        scanf("Qual opcao quer realizar: %d", &condition);
-        
+        scanf("%d", &condition);
+
+
         switch (condition){
         case 1:
-            printf("processando...\n");
+            sleep(1.5);
             break;
         case 2:
+            sleep(1.5);
             break;
         case 3:
+            sleep(1.5);
             break;
         case 4:
+            sleep(1.5);
             break;
         case 5:
+            sleep(1.5);
             break;
         case 6:
+            sleep(1.5);
             break;
         case 7:
+            sleep(1.5);
             break;
         case 9:
+            sleep(1.5);
             break;
         case 10:
+            sleep(1.5);
             break;
         case 11:
+            sleep(1.5);
             break;
         case 99:
             continuar = 0;
             break;
         default:
             printf("Nao tenho essa opcao.\n");
+            sleep(1.5);
             break;
         }
+        fflush(stdin);
     }
     return 0;
 }
+#endif
